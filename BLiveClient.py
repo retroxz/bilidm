22d1ece515f87b12892fc3a3cc97b51cd4fa1738
retroxz <zzxee666@gmail.com>
2021-01-29 01:49:31 +0800

    :sparkles: 捕获礼物和sc信息

--- BLiveClient.py
+++ BLiveClient.py
@@ -1,24 +1,23 @@
 import blivedm
 import logger
-# from model.Danmaku import Danmaku
+import time
 from model.Tables import BiliDanmaku
+from model.Tables import BiliGift
+from model.Tables import BiliGuard
+from model.Tables import BiliSc
 from config import BLive
 
 bili_danmaku = BiliDanmaku()
+bili_gift = BiliGift()
+bili_guard = BiliGuard()
+bili_sc = BiliSc()
+
+
+# 时间戳转字符串
+def timestamp_to_str(timestamp):
+    return time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(timestamp))
 
 class MyBLiveClient(blivedm.BLiveClient):
-    # danmaku_table = Danmaku()
-    # 演示如何自定义handler
-    # _COMMAND_HANDLERS = blivedm.BLiveClient._COMMAND_HANDLERS.copy()
-
-    """async def __on_vip_enter(self, command):
-        print(command)
-
-    # 老爷入场
-    _COMMAND_HANDLERS['WELCOME'] = __on_vip_enter
-
-    async def _on_receive_popularity(self, popularity: int):
-        print(f'当前人气值：{popularity}')"""
 
     # 弹幕回调
     async def _on_receive_danmaku(self, danmaku: blivedm.DanmakuMessage):
@@ -29,9 +28,9 @@ class MyBLiveClient(blivedm.BLiveClient):
         # 写入数据表
         params = danmaku.__dict__
         # 添加数据
-        params['medal_room_id'] = params['room_id'] # 勋章房间id
-        params['medal_runame'] = params['runame'] # 勋章房间主播名
-        params['medal_color'] = params['mcolor'] # 勋章颜色
+        params['medal_room_id'] = params['room_id']  # 勋章房间id
+        params['medal_runame'] = params['runame']  # 勋章房间主播名
+        params['medal_color'] = params['mcolor']  # 勋章颜色
 
         # 发送弹幕的房间信息
         params['room_id'] = BLive.roomid
@@ -44,19 +43,18 @@ class MyBLiveClient(blivedm.BLiveClient):
                             medal_room_id=params['medal_room_id'],
                             medal_runame=params['medal_runame'],
                             mobile_verify=params['mobile_verify'],
-                            msg = params['msg'],
-                            msg_type = params['msg_type'],
-                            privilege_type = params['privilege_type'],
-                            room_id = params['room_id'],
-                            runame = params['runame'],
-                            svip = params['svip'],
-                            timestamp = params['timestamp'],
-                            uid = params['uid'],
-                            uname = params['uname'],
-                            urank = params['urank'],
-                            user_level = params['user_level'],
-                            vip = params['vip'])
-
+                            msg=params['msg'],
+                            msg_type=params['msg_type'],
+                            privilege_type=params['privilege_type'],
+                            room_id=params['room_id'],
+                            runame=params['runame'],
+                            svip=params['svip'],
+                            timestamp=params['timestamp'],
+                            uid=params['uid'],
+                            uname=params['uname'],
+                            urank=params['urank'],
+                            user_level=params['user_level'],
+                            vip=params['vip'])
 
     # 礼物回调
     async def _on_receive_gift(self, gift: blivedm.GiftMessage):
@@ -64,12 +62,82 @@ class MyBLiveClient(blivedm.BLiveClient):
             f'[礼物] {gift.uname} 赠送{gift.gift_name}x{gift.num} （{gift.coin_type}币x{gift.total_coin}）')
         logger.write_json_str(gift.__dict__)
 
+        # 写入数据表
+        params = gift.__dict__
+        # 发送弹幕的房间信息
+        params['room_id'] = BLive.roomid
+        params['runame'] = BLive.runame
+
+        bili_gift.create(gift_name=params['gift_name'],
+                         gift_id=params['gift_id'],
+                         gift_type=params['gift_type'],
+                         num=params['num'],
+                         price=params['price'],
+                         action=params['action'],
+                         coin_type=params['coin_type'],
+                         total_coin=params['total_coin'],
+                         uid=params['uid'],
+                         uname=params['uname'],
+                         face=params['face'],
+                         guard_level=params['guard_level'],
+                         timestamp=timestamp_to_str(params['timestamp']),
+                         room_id=params['room_id'],
+                         runame=params['runame'])
+
     # 大航海回调
     async def _on_buy_guard(self, message: blivedm.GuardBuyMessage):
         logger.get_logger().warning(f'[舰团] {message.username} 购买{message.gift_name}')
         logger.write_json_str(message.__dict__)
 
+        # 写入数据表
+        params = message.__dict__
+        # 发送弹幕的房间信息
+        params['room_id'] = BLive.roomid
+        params['runame'] = BLive.runame
+
+        bili_guard.create(uid = params['uid'],
+        guard_level = params['guard_level'],
+        num = params['num'],
+        price = params['price'],
+        gift_id = params['gift_id'],
+        gift_name = params['gift_name'],
+        start_time = timestamp_to_str(params['start_time']),
+        end_time = timestamp_to_str(params['end_time']),
+        room_id = params['room_id'],
+        runame = params['runame'])
+
     # SC回调
     async def _on_super_chat(self, message: blivedm.SuperChatMessage):
         logger.get_logger().info(f'[SC]] ¥{message.price} {message.uname}：{message.message}')
         logger.write_json_str(message.__dict__)
+
+        # 写入数据表
+        params = message.__dict__
+        # 发送弹幕的房间信息
+        params['room_id'] = BLive.roomid
+        params['runame'] = BLive.runame
+
+
+        bili_sc.create(price = params['price'],
+        message = params['message'],
+        message_jpn = params['message_jpn'],
+        time = params['time'],
+        start_time = timestamp_to_str(params['start_time']),
+        end_time = timestamp_to_str(params['end_time']),
+        message_id = params['id'],
+        gift_id = params['gift_id'],
+        gift_name = params['gift_name'],
+        uid = params['uid'],
+        uname = params['uname'],
+        face = params['face'],
+        guard_level = params['guard_level'],
+        user_level = params['user_level'],
+        room_id = params['room_id'],
+        runame = params['runame'],
+        background_bottom_color = params['background_bottom_color'],
+        background_color = params['background_color'],
+        background_icon = params['background_icon'],
+        background_image = params['background_image'],
+        background_price_color = params['background_price_color'])
+
+
--- model/Tables.py
+++ model/Tables.py
@@ -44,4 +44,65 @@ class BiliDanmaku(BaseModel):
     user_level = IntegerField(null=True)
     vip = IntegerField(null=True)
 
+class BiliGift(BaseModel):
+    action = CharField(null=True)
+    coin_type = CharField(null=True)
+    face = CharField(null=True)
+    gift_id = IntegerField(null=True)
+    gift_name = CharField(null=True)
+    gift_type = IntegerField(null=True)
+    guard_level = IntegerField(null=True)
+    num = IntegerField(null=True)
+    price = IntegerField(null=True)
+    room_id = IntegerField(null=True)
+    runame = CharField(null=True)
+    timestamp = DateTimeField(null=True)
+    total_coin = IntegerField(null=True)
+    uid = IntegerField(null=True)
+    uname = CharField(null=True)
+
+    class Meta:
+        table_name = 'bili_gift'
+
+class BiliGuard(BaseModel):
+    end_time = DateTimeField(null=True)
+    gift_id = IntegerField(null=True)
+    gift_name = CharField(null=True)
+    guard_level = IntegerField(null=True)
+    num = IntegerField(null=True)
+    price = IntegerField(null=True)
+    room_id = IntegerField(null=True)
+    runame = CharField(null=True)
+    start_time = DateTimeField(null=True)
+    uid = IntegerField(null=True)
+
+    class Meta:
+        table_name = 'bili_guard'
+
+class BiliSc(BaseModel):
+    background_bottom_color = CharField(null=True)
+    background_color = CharField(null=True)
+    background_icon = CharField(null=True)
+    background_image = CharField(null=True)
+    background_price_color = CharField(null=True)
+    end_time = DateTimeField(null=True)
+    face = CharField(null=True)
+    gift_id = IntegerField(null=True)
+    gift_name = CharField(null=True)
+    guard_level = IntegerField(null=True)
+    message = CharField(null=True)
+    message_id = IntegerField(null=True)
+    message_jpn = CharField(null=True)
+    price = DecimalField(null=True)
+    room_id = IntegerField(null=True)
+    runame = CharField(null=True)
+    start_time = DateTimeField(null=True)
+    time = IntegerField(null=True)
+    uid = IntegerField()
+    uname = CharField(null=True)
+    user_level = IntegerField(null=True)
+
+    class Meta:
+        table_name = 'bili_sc'
+
 
